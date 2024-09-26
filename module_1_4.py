fcfce4329287f924281d043c811009f24fea041e
RyazanovAV <toser11@gmail.com>
2024-09-26 16:36:42 +0300

    Переменные

---
+++ module_1_4.py
@@ -0,0 +1,5 @@
+completed_homework = 12
+number_of_hours_spent = 1.5
+name_course = 'Python'
+print('Курс:', name_course+', всего задач:', str(completed_homework)+', затрачено часов:',
+      str(number_of_hours_spent)+', среднее время выполнения', number_of_hours_spent / completed_homework, 'часа.')
