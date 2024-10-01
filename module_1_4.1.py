d1b97d0da44fb16e76ddfd6762774959bdb3e624
RyazanovAV <toser11@gmail.com>
2024-10-01 10:05:40 +0300

    Организация программ и методы строк

---
+++ module_1_4.1.py
@@ -0,0 +1,15 @@
+my_string = input('Введите произвольный текст:')
+m_string = 'urban'
+
+print(len (my_string)) # Функция len озвращает длинну строки
+print(my_string.upper())
+print(my_string.lower())
+print(my_string[0])
+print(my_string [-1])
+print(my_string.replace(' ',''))
+
+#Эксперементы
+#last_str = (my_string.rfind(''))
+#print(last_str)
+#print(my_string [my_string.rfind('')-1])
+#print(my_string [my_string.rindex('')-1])
