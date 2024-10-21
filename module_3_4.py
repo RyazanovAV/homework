6401e0b4d9245d5a4c44fbac9e081232e0b1e74f
RyazanovAV <toser11@gmail.com>
2024-10-21 20:33:03 +0300

    Произвольное число параметров

---
+++ module_3_4.py
@@ -0,0 +1,21 @@
+def single_root_words (root_word,  *other_words):
+    same_words = []
+    for i in other_words:
+        i.lower
+        if len(root_word) <= len(i):
+            if i.lower().count(root_word.lower()) > 0:
+                same_words.append(i)
+        else:
+            if root_word.lower().count(i.lower()) > 0:
+                same_words.append(i)
+
+    return same_words
+
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+
+print(result1)
+
+print(result2)
